bca9c9f008d0954f8413be22ad3f028799bf48f1
karl kalvik <karl@domos.no>
2018-10-10 12:43:26 +0200

    sorted by price and normal distrebuted random selection

--- index.js
+++ index.js
@@ -82,6 +82,16 @@ rtm.on('reaction_added', (message) => {
     }
 });
 
+
+
+function compare(a, b) {
+    if (a.gross_unit_price < b.gross_unit_price)
+        return -1;
+    if (a.gross_unit_price > b.gross_unit_price)
+        return 1;
+    return 0;
+}
+
 rtm.on('message', (message) => {
     // For structure of `event`, see https://api.slack.com/events/message
 
@@ -117,6 +127,7 @@ rtm.on('message', (message) => {
                 function (body) {
                     var data = JSON.parse(body);
                     var foods = data.products;
+                    foods.sort(compare);
                     var selected = [];
                     for (var k = 0; k < Math.min(3, foods.length); k++) {
                         var ran = k;
@@ -181,8 +192,24 @@ function startDoneTimer(delay) {
 function getProductAsString(product) {
     return product.full_name + ' <' + product.front_url + '|' + product.gross_price + 'kr>'
 }
+function randn_bm() {
+    var u = 0, v = 0;
+    while (u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+    while (v === 0) v = Math.random();
+    let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
+    num = num / 10.0 + 0.5; // Translate to 0 -> 1
+    if (num > 1 || num < 0) return randn_bm(); // resample between 0 and 1
+    return num;
+}
+
+function abs_random() {
+    var rand = randn_bm();
+    rand -= 0.5;
+    rand = Math.abs(rand);
+    return rand;
+}
 function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
+    return Math.floor(abs_random() * Math.floor(max));
 }
 var commands = {
     pollDone: function () {
